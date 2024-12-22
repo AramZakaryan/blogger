9377e6be16a6ba018a2b910ee4b15598d649c760
Aram Zakaryan <aram.zakaryan@n.rich>
2024-12-23 00:43:03 +0400

    add headers validator

--- src/common/validators/headersValidator.ts
+++ src/common/validators/headersValidator.ts
@@ -1,16 +1,17 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const headersValidator = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.headers) {
   // if (req.headers['Authorization'] !== 'Basic YWRtaW46cXdlcnR5') {
-  //   res.status(401).json({
-  //     errorsMessages: [
-  //       {
-  //         message: 'headers required',
-  //         field: 'headers',
-  //       },
-  //     ],
-  //   })
-  //   return
-  // }
+    res.status(401).json({
+      errorsMessages: [
+        {
+          message: 'headers required',
+          field: 'headers',
+        },
+      ],
+    })
+    return
+  }
   next()
 }
