603392b0876da6cc62b7914cbdbb1155d4c4299d
Aram Zakaryan <aram.zakaryan@n.rich>
2024-12-23 00:13:28 +0400

    delete temporary headers validator

--- src/common/validators/headersValidator.ts
+++ src/common/validators/headersValidator.ts
@@ -1,16 +1,16 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const headersValidator = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.headers['x-custom-header']) {
-    res.status(401).json({
-      errorsMessages: [
-        {
-          message: 'headers required',
-          field: 'headers',
-        },
-      ],
-    })
-    return
-  }
+  // if (!req.headers['x-custom-header']) {
+  //   res.status(401).json({
+  //     errorsMessages: [
+  //       {
+  //         message: 'headers required',
+  //         field: 'headers',
+  //       },
+  //     ],
+  //   })
+  //   return
+  // }
   next()
 }
