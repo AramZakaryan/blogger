ed85335dfc8372f4acd0c2ce3d3d1153e7cd4113
Aram Zakaryan <aram.zakaryan@n.rich>
2024-12-22 13:13:19 +0400

    change paths (delete '/api')

--- src/common/paths.ts
+++ src/common/paths.ts
@@ -1,5 +1,8 @@
 export const PATHS = {
-  TESTING: '/api/testing/all-data',
-  BLOGS: '/api/blogs',
-  POSTS: '/api/posts',
+  TESTING: '/testing/all-data',
+  BLOGS: '/blogs',
+  POSTS: '/posts',
+  // TESTING: '/api/testing/all-data',
+  // BLOGS: '/api/blogs',
+  // POSTS: '/api/posts',
 }
